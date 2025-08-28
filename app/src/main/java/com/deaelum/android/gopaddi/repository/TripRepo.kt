7a66c696f54f542537580592ed00ae9ddd81950e
Tony Gabriel <tonyganriel@gmail.com>
2025-08-28 16:04:58 +0100

    Network Call

--- app/src/main/java/com/deaelum/android/gopaddi/repository/TripRepo.kt
+++ app/src/main/java/com/deaelum/android/gopaddi/repository/TripRepo.kt
@@ -6,23 +6,4 @@ import retrofit2.Response
 
 class TripRepo(private val tripApi: TripApi) {
 
-    suspend fun getAllTrips(): List<Trip> {
-        return tripApi.getAllTrips()
-    }
-
-    suspend fun saveTrip(trip: Trip): Trip {
-        return tripApi.saveTrip(trip)
-    }
-
-    suspend fun updateTrip(id: String, trip: Trip): Trip {
-        return tripApi.updateTrip(id, trip)
-    }
-
-    suspend fun deleteTrip(id: String): Response<Unit> {
-        return tripApi.deleteTrip(id)
-    }
-
-    suspend fun getTripById(id: String): Trip {
-        return tripApi.getTripById(id)
-    }
 }
